4f4a1ac55f1bdaacd09cb5a1aa01f852030a4ff0
mariajmac <mariajmag@hotmail.com>
2021-02-17 17:59:18 -0600

    Se agregaron librerias para crear el mapa

--- LimpiezaDatos.R
+++ LimpiezaDatos.R
@@ -3,28 +3,26 @@ library(readxl)
 library(tidyquant)
 library(stringr)
 library(cellranger)
+library(sf)
+library(ggplot2)
+library(leaflet)
+
 
 localidades <- read_xlsx("localidades.xlsx",col_names=c("is","zonas"),skip=1) %>%
   mutate(Apellidos = word(is,1,2)) %>%
-  relocate(Apellidos, .after = is) %>% 
-  mutate(Apellidos=ifelse(Apellidos =="DE LA",word(is,1,3),Apellidos),
-         Apellidos=ifelse(Apellidos == "LIZARAN MACEDA",word(is,1,3),Apellidos))
+  relocate(Apellidos, .after = is)  
 
-# verificación de 2 apellidos iguales
-n_occur <- data.frame(table(localidades$Apellidos))
-reps <- localidades[localidades$Apellidos %in% n_occur$Var1[n_occur$Freq > 1],]
- 
+datos <- read_xlsx("matriz_cursos.xlsx","Detalle de matriz",
+                   range = cell_cols("A:I"),
+)
 
-datos <- read_xlsx("matriz_cursos.xlsx","Detalle de matriz",range = cell_cols("A:I")) %>% 
+datos <- as_tibble(datos) %>%
   mutate(IS = gsub("[0-9]+", "", IS), IS = gsub("[[:punct:]]", "", IS)) %>%
   mutate(Apellidos = word(IS,-2,-1)) %>% 
   relocate(Apellidos, .after = IS) %>% 
-  select(everything(),-c("IS & Modelo","V")) 
-
-datos <- datos %>% 
-  mutate(Apellidos=ifelse(datos$Apellidos =="DE LA",word(IS,1,3),datos$Apellidos),
-         Apellidos=ifelse(datos$Apellidos == "LIZARAN MACEDA",paste(word(IS,-2,-1),word(IS,1)),datos$Apellidos))
-
+  select(everything(),-c("IS & Modelo","V"))# %>% 
+  #mutate(Zona = VLOOKUP(Apellidos,localidades,apellidos,zonas))
+  
 datos <- left_join(datos,localidades, by="Apellidos")
 
 datos <- datos %>% 
@@ -33,3 +31,20 @@ datos <- datos %>%
   drop_na(zonas) %>% 
   select(everything(),-Apellidos)
 
+
+
+
+ # mutate(datos, V = VLOOKUP(IS,localidades,IS,zonas))
+
+#MAPA
+
+# carga del plano base
+#leaflet()  %>%  addTiles() 
+
+names(datos)
+
+leaflet() %>% 
+  addTiles() %>% 
+  addMarkers(data = datos,
+             label = datos$zonas,
+             )
