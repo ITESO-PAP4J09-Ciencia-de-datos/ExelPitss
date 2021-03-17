17d54f806b5d411b458d3e72f6bd0d6cbd059566
Pablo <pbenavides@iteso.mx>
2021-03-17 12:50:37 -0600

    added

--- Tiempos_de_respuesta/eda_TR.R
+++ Tiempos_de_respuesta/eda_TR.R
@@ -1,32 +1,23 @@
 
-# librerías ---------------------------------------------------------------
+# librerÃ­as ---------------------------------------------------------------
 
 # ciencia de datos
 library(tidyverse)
 
-# importación de datos
+# importaciÃ³n de datos
 library(readxl)
 
-# compresión de listas
+# compresiÃ³n de listas
 library(comprehenr)
 
 # datos -------------------------------------------------------------------
 
-datos <- read_xlsx("tiempos_respuesta.xlsx")
+datos <- read_xlsx("Tiempos_de_respuesta/tiempos_respuesta.xlsx")
 
-cols.num <- c("Tiempo transcurrido", "Tiempo efectivo en sitio",
-              "Tiempo de respuesta", "Limite de tiempo de respuesta",
-              "Tiempo límite restante...24", "Tiempo efectivo", 
-              "Limite tiempo de solución total","Tiempo límite restante...27")
-datos[cols.num] <- sapply(datos[cols.num],as.numeric)
+glimpse(datos)
 
-minimos <- to_vec(for(i in 1:ncol(datos)) if(lapply(datos[,i],class)=="numeric") min(datos[,i]) else "NA")
-
-minimo <- if(lapply(datos[,23],class)=="numeric"){
-  min(datos[,23])
-}
-  
-
+datos <- datos %>% 
+  mutate(across(contains("Tiempo"), as.numeric))
 
 # EDA ---------------------------------------------------------------------
 
@@ -36,9 +27,9 @@ eda <- function(data){
     "Tipos de Datos" = as.character(as_tibble(sapply(datos, class))[1,]),
     "Datos Faltantes" = to_vec(for(i in 1:ncol(data)) sum(is.na(data[,i])) ),
     "Datos Presentes" = to_vec(for(i in 1:ncol(data)) sum(!is.na(data[,i])) ),
-    "Valores Únicos" = to_vec(for(i in 1:ncol(data)) count(distinct(data[,i]))),
-    "Valor Mínimo" = to_vec(for(i in 1:ncol(data)) if(lapply(data[,i],class)=="numeric") min(data[,i], na.rm = T) else "NA"),
-    "Valor Máximo" = to_vec(for(i in 1:ncol(data)) if(lapply(data[,i],class)=="numeric") max(data[,i], na.rm = T) else "NA"),
+    "Valores ?nicos" = to_vec(for(i in 1:ncol(data)) count(distinct(data[,i]))),
+    "Valor M?nimo" = to_vec(for(i in 1:ncol(data)) if(lapply(data[,i],class)=="numeric") min(data[,i], na.rm = T) else "NA"),
+    "Valor M?ximo" = to_vec(for(i in 1:ncol(data)) if(lapply(data[,i],class)=="numeric") max(data[,i], na.rm = T) else "NA"),
     "Media" = to_vec(for(i in 1:ncol(data)) if(lapply(data[,i],class)=="numeric") max(data[,i]) else "NA")
   )
   print(dqr)
