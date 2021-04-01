16dd63d0c003c839c1b488c6b67ffa02739d0dca
GabrielAAO <gabriel.antonioaguiar@hotmail.com>
2021-03-31 22:58:27 -0700

    Modelos

--- appBase.R
+++ appBase.R
@@ -9,7 +9,7 @@ library(plotly)
 library(patchwork)
 
 # funciones ---------------------------------------------------------------
-source("Limpieza.R", 
+source("limpieza_reporte_Tiempo_respuesta.R", 
        local = knitr::knit_global(),
        encoding = "utf-8")
 # DATA --------------------------------------------------------------------
@@ -26,7 +26,7 @@ modelo_I <- df %>%
 tecnico <- df %>% 
     distinct(`Técnico de visita`, Ruta) %>% 
     pull()
-
+md <- c("Drift","Naïve", "Seasonal Naïve", "Mean")
 #names(traccion) <- c("Delantera","4x4","Trasera")
 
 # UI ----------------------------------------------------------------------
@@ -46,7 +46,8 @@ ui <- fluidPage(
                           width    = "150px" 
                         ),
                       
-                      uiOutput(outputId = "tabla"),
+                      uiOutput(outputId = "tecnico"),
+                      uiOutput(outputId = "modelo"),
                       
                       #selectInput(
                           #inputId   = "Tecnico",
@@ -76,31 +77,77 @@ server <- function(input, output, session) {
           filter(Ruta == input$Estado)
     })
     
+    output$tecnico <- renderUI({
+      pickerInput(
+        inputId  = "Tecnico",
+        label    = "Escoge los tecnicos a nombrar",
+        choices  = df_filtrada() %>% 
+          distinct(`Técnico de visita`) %>% 
+          pull(`Técnico de visita`),
+        selected = FALSE,
+        options  = list(
+          `actions-box` = TRUE,
+          size = 5,
+          `selected-text-format` = "count > 3"
+        ),
+        multiple = TRUE,
+        width = "auto"
+      )
+    })
     
-    output$tabla <- renderTable({
-  })
+    modelos <- reactive({
+      Train_tsb %>% 
+        filter(Ruta == input$Estado)
+      
+      if (input$modelos == "Drift"){
+        modelos() <- Train_tsb %>%
+          model(Drift = RW(`Tiempo_de_respuesta`~drift())
+          ) %>% 
+          forecast(h = "1 month")
+      }
+      
+      if (input$modelos == "Naïve"){
+        modelos() <- Train_tsb %>%
+          model(NAIVE(`Tiempo_de_respuesta`)
+          ) %>% 
+          forecast(h = "1 month")
+      }
+      
+      if (input$modelos == "Seasonal Naïve"){
+        modelos() <- Train_tsb %>%
+          model(SNAIVE(`Tiempo_de_respuesta`)
+          ) %>% 
+          forecast(h = "1 month")
+      }
+      
+      if (input$modelos == "Mean"){
+        modelos() <- Train_tsb %>%
+          model(MEAN(`Tiempo_de_respuesta`)
+          ) %>% 
+          forecast(h = "1 month")
+      }
+      
+    })
     
+  
     
-    output$tabla <- renderUI({
-        pickerInput(
-            inputId  = "Tecnico",
-            label    = "Escoge los tecnicos a nombrar",
-            choices  = df_filtrada() %>% 
-              distinct(`Técnico de visita`) %>% 
-              pull(`Técnico de visita`),
-            selected = FALSE,
-            options  = list(
-                `actions-box` = TRUE,
-                size = 5,
-                `selected-text-format` = "count > 3"
-            ),
-            multiple = TRUE,
-            width = "auto"
+    output$modelo <- renderUI({
+      pickerInput(
+        inputId  = "modelos",
+        label    = "Escoge el modelo",
+        choices  = md,
+        selected = FALSE,
+        options  = list(
+          `actions-box` = TRUE,
+          size = 5,
+          `selected-text-format` = "count > 3"
         )
+      )
     })
     
     output$grafica <- renderPlot({
-      
+      autoplot(modelos())+
+        ggtitle("Modelo")
     })
 }
     shinyApp(ui, server)
