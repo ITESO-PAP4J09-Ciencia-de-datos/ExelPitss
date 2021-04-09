cfe2821204b387208a4aabbaadf622630dc9269e
Daniela Benavides <danii_bena98@live.com.mx>
2021-04-09 14:46:50 -0500

    cambios

--- Tiempos_de_respuesta/app.R
+++ Tiempos_de_respuesta/app.R
@@ -19,6 +19,9 @@ library(tsibble)
 library(feasts)
 library(fable)
 
+# visualización de datos
+library(plotly)
+
 # DATOS -------------------------------------------------------------------
 
 # COLORES
@@ -92,7 +95,28 @@ rutas <- datos %>%
     pull()
 
 frecuencias <- c("Diariamente","Semanalmente","Mensualmente")
-modelos <- c("MEAN","NAÏVE","SEASONAL NAÏVE","DRIFT","ETS", "ARIMA")
+
+modelos <- list(
+  Media = MEAN(Tiempos),
+  Ingenuo = NAIVE(Tiempos),
+  `Ingenuo Estacional` = SNAIVE(Tiempos),
+  Drift = RW(Tiempos ~ drift()),
+  `Suavización Exponencial` = ETS(Tiempos),
+  Arima = ARIMA(Tiempos)
+  
+)
+
+# training diarios
+tiempos_dtrain <- datos_tsbl %>% 
+    filter_index("2018-01-11" ~ "2020-03-10")
+
+# training semanales
+tiempos_wtrain <- datos_week_tsbl %>% 
+    filter_index("2018-01-11" ~ "2020-03-10")
+
+# training mensuales
+tiempos_mtrain <- datos_month_tsbl %>% 
+    filter_index("2018-01-11" ~ "2020-03-10")
 
 # UI CODE ----------------------------------------------------------------------
 
@@ -117,7 +141,8 @@ ui <- fluidPage(
                                      selected = "JALISCO"),
                          radioButtons("frecuencia1",
                                       label = h4("Frecuencia", style="color:#E27831; font-style:urw din italic; font-size:20px"),
-                                      choices = frecuencias),
+                                      choices = frecuencias,
+                                      selected = "Mensualmente"),
                          actionButton("go1","Visualizar los datos",
                                       icon = icon(name="eye"),
                                       style = "color: #fff; background-color: #F89438; border-color: #E27831")
@@ -146,12 +171,13 @@ ui <- fluidPage(
                                      selected = "JALISCO"),
                          radioButtons("frecuencia2",
                                       label = h4("Frecuencia", style="color:#E27831; font-style:urw din italic; font-size:20px"),
-                                      choices = frecuencias),
+                                      choices = frecuencias,
+                                      selected = "Mensualmente"),
                          selectInput("modelo2",
                                      label = h4("Modelo", style="color:#E27831; font-style:urw din italic; font-size:20px"), 
-                                     choices = modelos, 
+                                     choices = names(modelos), 
                                      multiple = TRUE, 
-                                     selected = modelos),
+                                     selected = "SEASONAL NAÏVE"),
                          actionButton("go2","Modelar",
                                       icon = icon(name="tools"),
                                       style = "color: #fff; background-color: #F89438; border-color: #E27831"),
@@ -192,12 +218,13 @@ ui <- fluidPage(
                                      selected = "JALISCO"),
                          radioButtons("frecuencia3",
                                       label = h4("Frecuencia", style="color:#E27831; font-style:urw din italic; font-size:20px"),
-                                      choices = frecuencias),
+                                      choices = frecuencias,
+                                      selected = "Mensualmente"),
                          selectInput("modelo3",
                                      label = h4("Modelo", style="color:#E27831; font-style:urw din italic; font-size:20px"), 
-                                     choices = modelos, 
-                                     multiple = FALSE, 
-                                     selected = "ETS"),
+                                     choices = names(modelos), 
+                                     multiple = TRUE, 
+                                     selected = "SEASONAL NAÏVE"),
                          sliderInput("forecast3",
                                      label = h4("Rango de pronóstico según la periodicidad escogida", style="color:#E27831; font-style:urw din italic; font-size:20px"),
                                      min = 1, max = 1000, value = 5),
@@ -232,7 +259,8 @@ server <- function(input, output) {
     # observer para mostrar la visualización de datos
     shiny::observeEvent(input$go1,{
             
-            plot_d <- datos_tsbl %>%
+            plot_d <- reactive({
+              datos_tsbl %>%
                 filter(Ruta == as.name(input$ruta1)) %>%
                 autoplot(Tiempos) + #aes(color = SLA) +
                 facet_wrap(~ SLA, scales = "free_y") +
@@ -243,8 +271,10 @@ server <- function(input, output) {
                     plot.title = element_text(family = '', colour="#000033", size=17, hjust = 0.5, vjust = 1)
                       ) + 
                 scale_colour_manual(values = c("#F89438","#009ACB","#03B2ED","#E27831"))
-            
-            plot_w <- datos_week_tsbl %>%
+            })
+              
+            plot_w <- reactive({
+              datos_week_tsbl %>%
                 filter(Ruta == as.name(input$ruta1)) %>%
                 autoplot(Tiempos) + #aes(color = SLA) +
                 facet_wrap(~ SLA, scales = "free_y") +
@@ -255,8 +285,10 @@ server <- function(input, output) {
                     plot.title = element_text(family = '', colour="#000033", size=17, hjust = 0.5, vjust = 1)
                     ) + 
                 scale_colour_manual(values = c("#F89438","#009ACB","#03B2ED","#E27831"))
-            
-            plot_m <- datos_month_tsbl %>%
+            })
+              
+            plot_m <- reactive({
+              datos_month_tsbl %>%
                 filter(Ruta == as.name(input$ruta1)) %>%
                 autoplot(Tiempos) +# aes(color = SLA) +
                 facet_wrap(~ SLA, scales = "free_y") +
@@ -267,114 +299,119 @@ server <- function(input, output) {
                     plot.title = element_text(family = '', colour="#000033", size=17, hjust = 0.5, vjust = 1)
                 ) + 
                 scale_colour_manual(values = c("#F89438","#009ACB","#03B2ED","#E27831"))
-            if (input$frecuencia1 == "Diariamente"){
-                output$grafica1 <- renderPlot({
-                    plot_d
-                })
-            } else if (input$frecuencia1 == "Semanalmente"){
-                output$grafica1 <- renderPlot({
-                    plot_w
-                })
-            } else {
-                output$grafica1 <- renderPlot({
-                    plot_m
-                })
-            }
-    })
-    
-    # observer para mostrar la visualización de datos
-    #shiny::observeEvent(input$go2,{
-        
-        # modelado diario
-        tiempos_dtrain <- datos_tsbl %>% 
-            filter_index("2018-01-11" ~ "2020-03-10")
-        tiempos_dtrain_fit <- tiempos_dtrain %>% 
-            filter(Ruta == input$ruta2) %>% 
-            model(Mean = MEAN(Tiempos),
-                  `Naïve` = NAIVE(Tiempos),
-                  `Seasonal naïve` = SNAIVE(Tiempos),
-                  Drift = RW(Tiempos ~ drift()),
-                  ETS = ETS(Tiempos),
-                  ARIMA = ARIMA(Tiempos))
-        tiempos_dtrain_fc <- tiempos_dtrain_fit %>% 
-            forecast(h = "1 year")
-        plot_dtrain_fc <- tiempos_dtrain_fc %>%
-            filter(.model %in% input$modelo2) %>%
-            autoplot(datos_tsbl, level = NULL) +
-            facet_wrap(~ SLA, scales = "free_y") +
-            theme_minimal() +
-            labs(x = "Días",title = "Pronósticos SLA's") +
-            guides(colour = guide_legend(title = "Pronóstico"))
-        
-        # modelado semanal
-        tiempos_wtrain <- datos_week_tsbl %>% 
-            filter_index("2018-01-11" ~ "2020-03-10")
-        tiempos_wtrain_fit <- tiempos_wtrain %>% 
-            filter(Ruta == input$ruta2) %>% 
-            model(Mean = MEAN(Tiempos),
-                  `Naïve` = NAIVE(Tiempos),
-                  `Seasonal naïve` = SNAIVE(Tiempos),
-                  Drift = RW(Tiempos ~ drift()),
-                  ETS = ETS(Tiempos),
-                  ARIMA = ARIMA(Tiempos))
-        tiempos_wtrain_fc <- tiempos_wtrain_fit %>% 
-            forecast(h = "1 year")
-        plot_wtrain_fc <- tiempos_wtrain_fc %>% 
-            filter(.model %in% input$modelo2) %>% 
-            autoplot(datos_week_tsbl, level = NULL) +
-            facet_wrap(~ SLA, scales = "free_y") +
-            theme_minimal() +
-            labs(x = "Semanas",title = "Pronósticos SLA's") +
-            guides(colour = guide_legend(title = "Pronóstico"))
-        
-        # modelado mensual
-        tiempos_mtrain <- datos_month_tsbl %>% 
-            filter_index("2018-01-11" ~ "2020-03-10")
-        tiempos_mtrain_fit <- tiempos_mtrain %>% 
-            filter(Ruta == input$ruta2) %>% 
-            model(Mean = MEAN(Tiempos),
-                  `Naïve` = NAIVE(Tiempos),
-                  `Seasonal naïve` = SNAIVE(Tiempos),
-                  Drift = RW(Tiempos ~ drift()),
-                  ETS = ETS(Tiempos),
-                  ARIMA = ARIMA(Tiempos))
-        tiempos_mtrain_fc <- tiempos_mtrain_fit %>% 
-            forecast(h = "1 year")
-        plot_mtrain_fc <- tiempos_mtrain_fc %>% 
-            filter(.model %in% input$modelo2) %>% 
-            autoplot(datos_month_tsbl, level = NULL) +
-            facet_wrap(~ SLA, scales = "free_y") +
-            theme_minimal() +
-            labs(x = "Meses",title = "Pronósticos SLA's") +
-            guides(colour = guide_legend(title = "Pronóstico"))
-        
-        # if para mostrar la correspondiente
-        if (input$frecuencia2 == "Diariamente"){
-            # output$reporte <- renderDataTable({
-            #     tiempos_dtrain_fit %>%  
-            #         report()
-            # })
-            output$grafica2 <- renderPlot({
-                plot_wtrain_fc
             })
             
+                output$grafica1 <- renderPlotly({
+                  if (input$frecuencia1 == "Diariamente"){
+                    plot_d()
+                  } else if (input$frecuencia1 == "Semanalmente"){
+                    plot_w()
+                  } else {
+                    plot_m()
+                  }
+                })
+    })
+    
+    # modelado diario
+    diarios_fit_filtrados <- reactive({
+      tiempos_dtrain %>% 
+        filter(Ruta == input$ruta2) %>%
+        model(modelos[[input$modelo2]])
+      # model(Mean = MEAN(Tiempos),
+      #       `Naïve` = NAIVE(Tiempos),
+      #       `Seasonal naïve` = SNAIVE(Tiempos),
+      #       Drift = RW(Tiempos ~ drift()),
+      #       ETS = ETS(Tiempos),
+      #       ARIMA = ARIMA(Tiempos))
+    }) 
+    
+    diarios_fc_filtrados <- reactive({
+      diarios_fit_filtrados() %>% 
+        forecast(h = "1 year")# %>% 
+      #select(input$modelo2)
+    })
+    
+    
+    plot_dtrain_fc <- reactive({
+      diarios_fc_filtrados() %>%
+        autoplot(datos_tsbl, level = NULL) +
+        facet_wrap(~ SLA, scales = "free_y") +
+        theme_minimal() +
+        labs(x = "Días",title = "Pronósticos SLA's") +
+        guides(colour = guide_legend(title = "Pronóstico"))
+    })
+    
+    # modelado semanal
+    semanal_fit_filtrados <- reactive({
+      tiempos_wtrain %>% 
+        filter(Ruta == input$ruta2) %>%
+        model(modelos[[input$modelo2]])
+      # model(Mean = MEAN(Tiempos),
+      #       `Naïve` = NAIVE(Tiempos),
+      #       `Seasonal naïve` = SNAIVE(Tiempos),
+      #       Drift = RW(Tiempos ~ drift()),
+      #       ETS = ETS(Tiempos),
+      #       ARIMA = ARIMA(Tiempos))
+    }) 
+    
+    semanal_fc_filtrados <- reactive({
+      semanal_fit_filtrados() %>% 
+        forecast(h = "1 year")# %>% 
+      #select(input$modelo2)
+    })
+    
+    plot_wtrain_fc <- reactive({
+      semanal_fc_filtrados() %>% 
+        autoplot(datos_week_tsbl, level = NULL) +
+        facet_wrap(~ SLA, scales = "free_y") +
+        theme_minimal() +
+        labs(x = "Semanas",title = "Pronósticos SLA's") +
+        guides(colour = guide_legend(title = "Pronóstico"))
+    }) 
+    
+    # modelado mensual
+    mensual_fit_filtrados <- reactive({
+      tiempos_mtrain %>% 
+        filter(Ruta == input$ruta2) %>%
+        model(modelos[[input$modelo2]])
+        # model(Mean = MEAN(Tiempos),
+        #       `Naïve` = NAIVE(Tiempos),
+        #       `Seasonal naïve` = SNAIVE(Tiempos),
+        #       Drift = RW(Tiempos ~ drift()),
+        #       ETS = ETS(Tiempos),
+        #       ARIMA = ARIMA(Tiempos))
+    })
+      
+    mensual_fc_filtrados <- reactive({
+      mensual_fit_filtrados() %>% 
+        forecast(h = "1 year")# %>% 
+        #select(input$modelo2)
+    })
+      
+    
+    plot_mtrain_fc <- reactive({
+      mensual_fc_filtrados() %>% 
+        autoplot(datos_month_tsbl, level = NULL) +
+        facet_wrap(~ SLA, scales = "free_y") +
+        theme_minimal() +
+        labs(x = "Meses",title = "Pronósticos SLA's") +
+        guides(colour = guide_legend(title = "Pronóstico"))
+    })
+          
+    output$grafica2 <- renderPlotly({
+        if (input$frecuencia2 == "Diariamente"){
+            plot_wtrain_fc()
         } else if (input$frecuencia2 == "Semanalmente"){
-            # output$reporte <- renderDataTable({
-            #     tiempos_wtrain_fit %>%
-            #         report()
-            # })
-            output$grafica2 <- renderPlot({
-                plot_wtrain_fc
-            })
+            plot_wtrain_fc()
         } else {
-            # output$reporte <- renderDataTable({
-            #     tiempos_mtrain_fit %>% 
-            #         report()
-            # })
-            output$grafica2 <- renderPlot({
-                plot_mtrain_fc
-            })
+            plot_mtrain_fc()
         }
+                
+    })
+            
+         
+            
+        
         
     #})
     
