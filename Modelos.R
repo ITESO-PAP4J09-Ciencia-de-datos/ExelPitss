6deeb818693d232802422659445e9b76bf564ef6
Pablo <pbenavides@iteso.mx>
2021-04-14 12:41:48 -0500

    Se agregan modelos.

--- Modelos.R
+++ Modelos.R
@@ -15,15 +15,15 @@ Train_tsb <- tiempos_os_tidy_tbl %>%
          Tiempos == "Tiempo de respuesta") %>%
   group_by(Ruta, Tiempos) %>%
   summarise_by_time(
-    .date_var    = Fecha_recepion,
-    .by          = "week",
-    Tiempo_de_respuesta = mean(hora_decimal))%>% 
+    .date_var           = Fecha_recepion,
+    .by                 = "week",
+    Tiempo_de_respuesta = mean(hora_decimal),
+    Q_OS                = n())%>% 
   ungroup() %>% 
   mutate(Fecha_recepion = yearweek(Fecha_recepion))%>%
   #mutate(Tiempo_de_respuesta = difference(Tiempo_de_respuesta)) %>% 
   as_tsibble(index = Fecha_recepion,
              key = c(Ruta, Tiempos))
-
 
 
 # Entrenamiento del modelo ------------------------------------------------
@@ -38,15 +38,46 @@ lambda1 <- Train_tsb %>%
 Modelos_fit <- Train_tsb %>% 
   filter(Ruta == "JALISCO", Fecha_recepion < yearweek("2020-11-15")) %>% 
   model(
-   "ARIMA" = ARIMA(Tiempo_de_respuesta),
-   "ARIMA_BC" = ARIMA(box_cox(Tiempo_de_respuesta,lambda1)),
-   "ARIMA_auto" = ARIMA(Tiempo_de_respuesta, stepwise = FALSE, approx = FALSE),
+   "ARIMA"        = ARIMA(Tiempo_de_respuesta),
+   "ARIMA_BC"     = ARIMA(box_cox(Tiempo_de_respuesta,lambda1)),
+   "ARIMA_auto"   = ARIMA(Tiempo_de_respuesta, stepwise = FALSE, approx = FALSE),
    "ARIMA_autoBC" = ARIMA(box_cox(Tiempo_de_respuesta, lambda1), 
-                          stepwise = FALSE, approx = FALSE)
+                          stepwise = FALSE, approx = FALSE),
+   ARIMA_fourier1    = ARIMA(Tiempo_de_respuesta ~ pdq(d = 1) + PDQ(0,0,0) + fourier(K = 1)),
+   ARIMA_fourier2    = ARIMA(Tiempo_de_respuesta ~ pdq(d = 1) + PDQ(0,0,0) + fourier(K = 2)),
+   ARIMA_fourier2BC  = ARIMA(box_cox(Tiempo_de_respuesta, lambda1) ~ pdq(d = 1) + PDQ(0,0,0) + fourier(K = 2)),
+   ARIMA_fourier3    = ARIMA(Tiempo_de_respuesta ~ pdq(d = 1) + PDQ(0,0,0) + fourier(K = 3)),
+   ARIMA_fourier4    = ARIMA(Tiempo_de_respuesta ~ pdq(d = 1) + PDQ(0,0,0) + fourier(K = 4)),
+   ARIMA_fourier5    = ARIMA(Tiempo_de_respuesta ~ pdq(d = 1) + PDQ(0,0,0) + fourier(K = 5)),
+   ARIMA_fourier6    = ARIMA(Tiempo_de_respuesta ~ pdq(d = 1) + PDQ(0,0,0) + fourier(K = 6)),
+   ARIMA213          = ARIMA(Tiempo_de_respuesta ~ pdq(2, 1, 3) + PDQ(0,0,0)),
+   dynamic_reg       = ARIMA(box_cox(Tiempo_de_respuesta, lambda1) ~ pdq(d = 1) + PDQ(0,0,0) + fourier(K = 2) + Q_OS)
+  ) %>% 
+  mutate(
+    Combinado = (ARIMA_fourier2 + ARIMA_auto + ARIMA213)/3
   )
-  
+
+Q_OS_last_year <- Train_tsb %>% 
+  filter(Ruta == "JALISCO", Fecha_recepion < yearweek("2020-11-15")) %>% 
+  filter_index("2019 W46" ~ "2019 W49") %>% pull(Q_OS)
+
+escenarios <- scenarios(
+  last_year = new_data(Train_tsb %>% 
+                         filter(Ruta == "JALISCO", 
+                                Fecha_recepion < yearweek("2020-11-15")), 4) %>% 
+    mutate(Q_OS = Q_OS_last_year),
+  real_val  = new_data(Train_tsb %>% 
+                         filter(Ruta == "JALISCO", 
+                                Fecha_recepion < yearweek("2020-11-15")), 4) %>% 
+    mutate(Q_OS = Train_tsb %>% filter(Ruta == "JALISCO") %>%  
+             filter_index("2020 W46" ~ "2020 W49") %>% pull(Q_OS))
+)
+
 Modelos_fc <- Modelos_fit %>% 
-  forecast(h = "1 month")
+  forecast(new_data = escenarios)
+
+# Modelos_fc <- Modelos_fit %>% 
+#   forecast(h = "1 month")
 
 
 # Grafica de las predicciones contra los datos
