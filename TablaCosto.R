e9d00e524654bc48a0d3166640902bc76ccae6be
mariajmac <42419964+mariajmac@users.noreply.github.com>
2021-05-13 20:40:17 -0500

    Se agregaron archivos

---
+++ TablaCosto.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(readxl)
+library(readxlsx)
+library(tidyquant)
+library(stringr)
+library(cellranger)
+library(sf)
+library(ggplot2)
+library(leaflet)
+library(raster)
+library(dplyr)
+library(spData)
+library(tmap)
+library(ggplot2)
+library(shiny)
+library(lubridate)
+
+tabla <- read_xlsx("CostoPorTecnico.xlsx")
+
+
